808f10c95e1322274188be7c5c1fac98d76f8330
Daniel Fogg <dan@inmoji.com>
2015-09-30 14:51:20 -0400

    add a demonstration of how to do this using a uibutton

--- UIContinuousForceTouchGestureRecognizer/UIContinuousForceTouchGestureRecognizer/ViewController.m
+++ UIContinuousForceTouchGestureRecognizer/UIContinuousForceTouchGestureRecognizer/ViewController.m
@@ -11,9 +11,10 @@
 
 @interface ViewController ()<UIContinuousForceTouchDelegate>
 
-
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (strong, nonatomic) UIContinuousForceTouchGestureRecognizer* forceTouchRecognizer;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (strong, nonatomic) UIContinuousForceTouchGestureRecognizer* imageForceTouchRecognizer;
+@property (strong, nonatomic) UIContinuousForceTouchGestureRecognizer* buttonForceTouchRecognizer;
 @property (strong, nonatomic) UITapGestureRecognizer* tapGestureRecognizer;
 @end
 
@@ -22,18 +23,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    _forceTouchRecognizer = [[UIContinuousForceTouchGestureRecognizer alloc] init];
-    _forceTouchRecognizer.timeout = 0.5f;
-    _forceTouchRecognizer.forceTouchDelay = 0.2f;
-    _forceTouchRecognizer.baseForceTouchPressure = 3.0f;
-    _forceTouchRecognizer.triggeringForceTouchPressure = 6.0f;
-    _forceTouchRecognizer.forceTouchDelegate = self;
+    _imageForceTouchRecognizer = [[UIContinuousForceTouchGestureRecognizer alloc] init];
+    _imageForceTouchRecognizer.timeout = 0.5f;
+    _imageForceTouchRecognizer.forceTouchDelay = 0.2f;
+    _imageForceTouchRecognizer.baseForceTouchPressure = 3.0f;
+    _imageForceTouchRecognizer.triggeringForceTouchPressure = 6.0f;
+    _imageForceTouchRecognizer.forceTouchDelegate = self;
+    
+    _buttonForceTouchRecognizer = [[UIContinuousForceTouchGestureRecognizer alloc] init];
+    _buttonForceTouchRecognizer.timeout = 0.5f;
+    _buttonForceTouchRecognizer.forceTouchDelay = 0.2f;
+    _buttonForceTouchRecognizer.baseForceTouchPressure = 3.0f;
+    _buttonForceTouchRecognizer.triggeringForceTouchPressure = 6.0f;
+    _buttonForceTouchRecognizer.forceTouchDelegate = self;
     
     //here to demonstrate how this works alonside a tap gesture recognizer
     _tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped:)];
     
     [self.imageView addGestureRecognizer:_tapGestureRecognizer];
-    [self.imageView addGestureRecognizer:_forceTouchRecognizer];
+    [self.imageView addGestureRecognizer:_imageForceTouchRecognizer];
+    
+    [self.button addGestureRecognizer:_buttonForceTouchRecognizer];
+    [self.button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -43,11 +54,20 @@
 }
 
 #pragma mark -
-#pragma UITapGestureRecognizer selector
+#pragma UIButton target selector
+
+- (void) buttonTapped:(id)sender {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [[[UIAlertView alloc] initWithTitle:@"Button Tap" message:@"YEAH!!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+    });
+}
+
+#pragma mark -
+#pragma UITapGestureRecognizer target selector
 
 - (void) tapped:(id)sender {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [[[UIAlertView alloc] initWithTitle:@"Tap" message:@"YEAH!!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+        [[[UIAlertView alloc] initWithTitle:@"Image Tap" message:@"YEAH!!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
     });
 }
 
@@ -56,39 +76,73 @@
 
 //required
 - (void) forceTouchRecognized:(UIContinuousForceTouchGestureRecognizer*)recognizer {
-    self.imageView.transform = CGAffineTransformIdentity;
-    [self.imageView setNeedsDisplay];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [[[UIAlertView alloc] initWithTitle:@"Force Touch" message:@"YEAH!!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
-    });
+    if(recognizer == _imageForceTouchRecognizer) {
+        self.imageView.transform = CGAffineTransformIdentity;
+        [self.imageView setNeedsDisplay];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [[[UIAlertView alloc] initWithTitle:@"Force Touch Image" message:@"YEAH!!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+        });
+    } else {
+        self.button.transform = CGAffineTransformIdentity;
+        [self.button setNeedsDisplay];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [[[UIAlertView alloc] initWithTitle:@"Force Touch Button" message:@"YEAH!!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+        });
+    }
 }
 
 //optional
 - (void) forceTouchRecognizer:(UIContinuousForceTouchGestureRecognizer*)recognizer didStartWithForce:(CGFloat)force maxForce:(CGFloat)maxForce {
     CGFloat transformDelta = 1.0f + ((force/maxForce) / 3.0f);
-    self.imageView.transform = CGAffineTransformMakeScale(transformDelta, transformDelta);
-    [self.imageView setNeedsDisplay];
+    if(recognizer == _imageForceTouchRecognizer) {
+        self.imageView.transform = CGAffineTransformMakeScale(transformDelta, transformDelta);
+        [self.imageView setNeedsDisplay];
+    } else {
+        self.button.transform = CGAffineTransformMakeScale(transformDelta, transformDelta);
+        [self.button setNeedsDisplay];
+    }
 }
 
 - (void) forceTouchRecognizer:(UIContinuousForceTouchGestureRecognizer*)recognizer didMoveWithForce:(CGFloat)force maxForce:(CGFloat)maxForce {
     CGFloat transformDelta = 1.0f + ((force/maxForce) / 3.0f);
-    self.imageView.transform = CGAffineTransformMakeScale(transformDelta, transformDelta);
-    [self.imageView setNeedsDisplay];
+    if(recognizer == _imageForceTouchRecognizer) {
+        self.imageView.transform = CGAffineTransformMakeScale(transformDelta, transformDelta);
+        [self.imageView setNeedsDisplay];
+    } else {
+        self.button.transform = CGAffineTransformMakeScale(transformDelta, transformDelta);
+        [self.button setNeedsDisplay];
+    }
 }
 
 - (void) forceTouchRecognizer:(UIContinuousForceTouchGestureRecognizer*)recognizer didCancelWithForce:(CGFloat)force maxForce:(CGFloat)maxForce  {
-    self.imageView.transform = CGAffineTransformIdentity;
-    [self.imageView setNeedsDisplay];
+    if(recognizer == _imageForceTouchRecognizer) {
+        self.imageView.transform = CGAffineTransformIdentity;
+        [self.imageView setNeedsDisplay];
+    } else {
+        self.button.transform = CGAffineTransformIdentity;
+        [self.button setNeedsDisplay];
+    }
+    
 }
 
 - (void) forceTouchRecognizer:(UIContinuousForceTouchGestureRecognizer*)recognizer didEndWithForce:(CGFloat)force maxForce:(CGFloat)maxForce  {
-    self.imageView.transform = CGAffineTransformIdentity;
-    [self.imageView setNeedsDisplay];
+    if(recognizer == _imageForceTouchRecognizer) {
+        self.imageView.transform = CGAffineTransformIdentity;
+        [self.imageView setNeedsDisplay];
+    } else {
+        self.button.transform = CGAffineTransformIdentity;
+        [self.button setNeedsDisplay];
+    }
 }
 
 - (void) forceTouchDidTimeout:(UIContinuousForceTouchGestureRecognizer*)recognizer {
-    self.imageView.transform = CGAffineTransformIdentity;
-    [self.imageView setNeedsDisplay];
+    if(recognizer == _imageForceTouchRecognizer) {
+        self.imageView.transform = CGAffineTransformIdentity;
+        [self.imageView setNeedsDisplay];
+    } else {
+        self.button.transform = CGAffineTransformIdentity;
+        [self.button setNeedsDisplay];
+    }
 }
 
 
